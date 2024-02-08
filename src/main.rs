d1d8a371db18dc8f6e245bb43a33eb870a781b0c
samuel mignot <sjmignot@gmail.com>
2024-02-08 12:02:33 -0800

    finish setting up basic grep

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,63 @@
+use clap::Parser;
+use colored::Colorize;
+use regex::Regex;
 use std::cmp::min;
 use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
-use regex::Regex;
-use clap::Parser;
+use std::io;
+use std::io::{BufRead, BufReader, Read};
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+// #[command(version, about, long_about = None)]
 struct Args {
     pattern: String,
 
     #[arg(short, long, default_value_t = 2)]
     context_lines: usize,
 
-    #[arg(short, long, default_value_t = String::from("-"))]
-    file: String
+    #[arg(short, long)]
+    file: Option<String>,
+}
+
+fn process_lines<R: Read>(reader: R) -> Vec<(usize, String)> {
+    let buffer = BufReader::new(reader);
+    buffer
+        .lines()
+        .enumerate()
+        .map(|(i, r)| (i, r.unwrap()))
+        .collect::<Vec<(usize, String)>>()
 }
 
 fn main() {
     let args = Args::parse();
     let pattern = args.pattern;
     let context_lines = args.context_lines;
-    let input = args.file;
 
     let needle = Regex::new(&pattern).unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let search_vec: Vec<(usize, String)> = match args.file {
+        None => process_lines(io::stdin()),
+        Some(i) => process_lines(File::open(i).unwrap()),
+    };
 
-    let mut ctx: Vec<&[(usize, String)]> = vec!{};
-    let search_vec = reader.lines().enumerate().map(|(i, r)| {(i, r.unwrap())}).collect::<Vec<(usize, String)>>();
+    let mut ctx: Vec<&[(usize, String)]> = vec![];
     let num_lines = search_vec.len();
+    println!("Searching for {:?} in text..\n", pattern);
 
     for (i, line) in search_vec.iter() {
-        match needle.find(line) {
-            Some(_) => {
-                ctx.push(
-                    &search_vec[i.saturating_sub(context_lines)..min(i.saturating_add(context_lines)+1, num_lines)]
-                );
-            },
-            None => ()
+        if needle.find(line).is_some() {
+            ctx.push(
+                &search_vec[i.saturating_sub(context_lines)
+                    ..min(i.saturating_add(context_lines) + 1, num_lines)],
+            );
         }
     }
     for ls in ctx.iter() {
-        for (i, l) in ls.iter() {
-            println!("{i}: {l}");
+        for (j, (i, l)) in ls.iter().enumerate() {
+            if j == context_lines {
+                println!("{}: {}", i.to_string().red().bold(), l);
+            } else {
+                println!("{}: {}", i, l);
+            }
         }
+        println!("\n")
     }
 }
